5b7ee3d5a30bb55f44ffc65c2d0ab7b765b827cb
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-22 11:20:13 +0900

    Gradlewビルド成功(Java17環境)

---
+++ server/src/main/java/com/example/api/ApiApplication.java
@@ -0,0 +1,21 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
