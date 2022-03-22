701d5012296b4aedf7f5e02fdc89eff708dfb1d8
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-22 15:23:15 +0900

    タイムリーフページ訂正

--- server/src/main/java/com/example/api/ApiApplication.java
+++ server/src/main/java/com/example/api/ApiApplication.java
@@ -3,20 +3,20 @@ package com.example.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Request Mappingは、Spring MVC のコントローラに付与して、リクエスト URL に対して、どのメソッドが処理を実行するか定義するアノテーション。
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 // RestControllerは、JsonやXML等を返すWebAPI用のコントローラ
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+// @RestController
 public class ApiApplication {
 
-	@RequestMapping("/")
+	// @RequestMapping("/")
 	// Viewに渡したいオブジェクトを設定する。
 	// homeメッドには、@RequestMapping アノテーションを付与し、クライアントからのリクエストを処理することを宣言している。
-	public String home() {
-		return "Hello World";
-	}
+	// public String home() {
+	// 	return "Hello World";
+	// }
 
 	// public  :どこからでも参照可能
 	// static  :インスタンス可しなくても外部から使用可能
